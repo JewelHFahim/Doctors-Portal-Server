8a8eb78d89d2fc64f23b996be7c9eb1125a483d4
JewelHFahim <jewelhfahim@gmail.com>
2022-11-16 00:41:58 +0600

    server data connected

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.qez1k8e.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const uri = `mongodb+srv://${process.env.DP_USER}:${process.env.DP_PASSWORD}@cluster0.qez1k8e.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const appointmentOptionCollection = client
+      .db("doctorsPortal")
+      .collection("appointmentOptions");
 
+    app.get("/appointmentoptions", async (req, res) => {
+      const query = {};
+      const cursor = appointmentOptionCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 
-app.get('/', (req, res)=>{
-    res.send('Doctors Portal Working Properly...')
-} )
-app.listen(port, ()=>{
-    console.log('Doctors Portal:', port);
-})
+app.get("/", (req, res) => {
+  res.send("Doctors Portal Working Properly...");
+});
+app.listen(port, () => {
+  console.log("Doctors Portal:", port);
+});
