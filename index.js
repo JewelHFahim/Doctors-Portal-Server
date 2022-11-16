900a70a1e5de12688a4ad70636c72927275a6d5b
JewelHFahim <jewelhfahim@gmail.com>
2022-11-16 19:05:32 +0600

    pass updated

--- index.js
+++ index.js
@@ -17,16 +17,55 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const appointmentOptionCollection = client
-      .db("doctorsPortal")
-      .collection("appointmentOptions");
+    const appointmentOptionCollection = client.db("doctorsPortal").collection("appointmentOptions");
+    const bookingsCollection = client.db("doctorsPortal").collection("bookings");
 
     app.get("/appointmentoptions", async (req, res) => {
+      const date = req.query.date;
       const query = {};
-      const cursor = appointmentOptionCollection.find(query);
+      const options = await appointmentOptionCollection.find(query).toArray();
+
+// get the booking of the provided date
+      const bookingQuery = {appointmentDate: date};
+      const alreadyBooked  = await bookingsCollection.find(bookingQuery).toArray();
+
+       options.forEach(option =>{
+        const optionBooked = alreadyBooked.filter(book => book.treatment === option.name );
+        const bookedSlots = optionBooked.map( book => book.slot);
+        const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+        option.slots = remainingSlots;
+        // console.log(date, option.name, remainingSlots.length);
+       })
+      res.send(options);
+    });
+
+    // Bookings
+    app.post('/bookings', async(req, res)=>{
+      const bookings = req.body;
+      const query = {
+        appointmentDate: bookings.appointmentDate,
+        email: bookings.email,
+        treatment: bookings.treatment
+      }
+
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
+      if(alreadyBooked.length){
+        const message = `You already have a booking ${bookings.appointmentDate}`;
+        return res.send({acknowledged: false, message});
+      }
+
+      const result = await bookingsCollection.insertOne(bookings);
+      res.send(result);
+    })
+
+    app.get('/bookings', async(req, res)=>{
+      const query = {};
+      const cursor = bookingsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    });
+    })
+
+
   } finally {
   }
 }
